3e64c638282118511dfeb5aea1343c220f60e89a
cmartingon <c.marting@udc.es>
2021-11-08 07:25:56 +0100

    implementacion userService

---
+++ PracticaMaD/Model/UserService/Exceptions/IncorrectPasswordException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Es.Udc.DotNet.ModelUtil.Log;
+
+namespace Es.Udc.DotNet.MiniPortal.Model.UserService.Exceptions
+{
+
+    /// <summary>
+    /// Public <c>ModelException</c> which captures the error 
+    /// with the passwords of the users.
+    /// </summary>
+    [Serializable]
+    public class IncorrectPasswordException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="IncorrectPasswordException"/> class.
+        /// </summary>
+        /// <param name="userName"><c>userName</c> that causes the error.</param>
+        public IncorrectPasswordException(String userName)
+            : base("Incorrect password exception => userName = " + userName)
+        {
+            this.userName = userName;
+        }
+
+        /// <summary>
+        /// Stores the User login name of the exception
+        /// </summary>
+        /// <value>The name of the login.</value>
+        public String userName { get; private set; }
+    }
+}
---
+++ PracticaMaD/Model/UserService/IUserService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Collections.Generic;
+using Ninject;
+using Es.Udc.DotNet.Photogram.Model.UserDao;
+using Es.Udc.DotNet.Photogram.Model.Dtos;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using Es.Udc.DotNet.ModelUtil.Transactions;
+
+namespace Es.Udc.DotNet.Photogram.Model.UserService
+{
+
+    public interface IUserService
+
+    {
+        [Inject]
+        IUserDao UserDao { set; }
+        /// <exception cref="InstanceNotFoundException"/>
+        /// <exception cref="IncorrectPasswordException"/>
+        [Transactional]
+        LoginResult Login(String userName, String password, Boolean passwordIsEncrypted);
+
+        [Transactional]
+        /// <exception cref="DuplicateInstanceException"/>
+        long RegisterUser(string userName, string password, string firstName, 
+            string lastName, string lastName2, string email, string country, string language);
+
+        [Transactional]
+        void BorrarCuenta(long userId);
+    }
+}
